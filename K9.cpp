699d126315ae678059ce364bf8eec8fa891ef1e9
MHS <gek@katherine>
2021-04-01 05:17:22 -0400

    Automatic commit.

--- K9.cpp
+++ K9.cpp
@@ -3,4 +3,5 @@
 #include <iostream>
 int main(){
 	std::cout << "hello world!" << std::endl;
+	myfunc();
 }
--- K9.hpp
+++ K9.hpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <ctgmath>
 #include <type_traits>
+#include <iostream>
 
 //Implementation variables.
 #define K9BLOCKSIZE ((ssize_t)1<<19)
@@ -13,23 +14,123 @@
 #define K9_USE_DOUBLE 1
 #define K9_USE_LONG_DOUBLE 1
 #define K9_IMPLEMENTATION_BITS 64
-typedef unsigned char BYTE;
 
 
 
-/*
-template <size_t size>
-class Membacker{
-	public:
-		virtual BYTE read(size_t where);
-		virtual void write(uint8_t byte, size_t where);
-		void copy(Membacker<size>& other){
-			for(ssize_t i = 0; i < (1<<(size-1)); i++){write(other.read(i),i);}
+
+//Signed integer implementation.
+template <typename Ti, typename To>
+struct int_intern{
+
+	private:
+		Ti f;
+		static_assert(sizeof(Ti) == sizeof(To), "Bad integer types");	
+		static inline int_intern<Ti, To> nonzero(int_intern<Ti, To> a){
+			if(a.__getintern() == 0)
+				a.__setintern(0);
+			return a;
 		}
-		virtual ~Membacker();
+	public:	
+
+#define K9_INTERNAL_INT_CONSTRUCTOR(x)\
+		inline int_intern<Ti, To>(x a) {f = a;}\
+		inline int_intern<Ti, To> operator=(x a) {f = a; return *this;}
+		//operator std::size_t() const {return f;}
+		inline int_intern<Ti, To>() = default;
+		inline int_intern<Ti, To> operator=(const int_intern<Ti, To> other) 		
+			{f = other.__getintern(); return *this;}
+		inline Ti __getintern() const
+			{return 	f;}
+		inline To __getinterno() const
+			{return (To)(f);}
+		void __setintern(Ti a)
+			{f = a;}
+		void __setinterno(To a)
+			{f = (Ti)(a);}
+	//Actual operator implementations.
+#define K9_INTERNAL_INT_SIMP_OP_NOEQ(x) \
+		int_intern<Ti, To> operator x(const int_intern<Ti, To> c) const {\
+			return __getintern() x c.__getintern();\
+		}
+#define K9_INTERNAL_INT_SIMP_OP(x) \
+		K9_INTERNAL_INT_SIMP_OP_NOEQ(x)\
+		int_intern<Ti, To> operator x##=(const int_intern<Ti, To> c){*this = *this x c; return *this;}
+#define K9_INTERNAL_INT_CMP_OP_NOEQ(x) \
+		bool operator x(const int_intern<Ti, To> c) const {\
+			return __getinterno() x (c).__getinterno();\
+		}
+#define K9_INTERNAL_INT_CMP_OP(x) \
+		K9_INTERNAL_INT_CMP_OP_NOEQ(x)\
+		bool operator x##=(const int_intern<Ti, To> c){bool result = *this x c; return result;}
+#define K9_INTERNAL_INT_DIV_OP(x) \
+		int_intern<Ti, To> operator x(const int_intern<Ti, To> c) const {\
+			return __getinterno() x nonzero(c).__getinterno();\
+		}\
+		int_intern<Ti, To> operator x##=(const int_intern<Ti, To> c) {*this = *this x c; return *this;}
+		K9_INTERNAL_INT_SIMP_OP(+)
+		K9_INTERNAL_INT_SIMP_OP(-)
+		K9_INTERNAL_INT_SIMP_OP(*)
+		K9_INTERNAL_INT_SIMP_OP(&)
+		K9_INTERNAL_INT_SIMP_OP(|)
+		K9_INTERNAL_INT_SIMP_OP(^)
+		K9_INTERNAL_INT_SIMP_OP(<<)
+		K9_INTERNAL_INT_SIMP_OP(>>)
+		K9_INTERNAL_INT_SIMP_OP_NOEQ(||)
+		K9_INTERNAL_INT_SIMP_OP_NOEQ(&&)
+		K9_INTERNAL_INT_CMP_OP_NOEQ(==)
+		K9_INTERNAL_INT_CMP_OP_NOEQ(!=)
+		K9_INTERNAL_INT_CMP_OP(>)
+		K9_INTERNAL_INT_CMP_OP(<)
+		K9_INTERNAL_INT_DIV_OP(/)
+		K9_INTERNAL_INT_DIV_OP(%)
+		int_intern<Ti, To> operator++(){this += 1; return *this;}
+		int_intern<Ti, To> operator--(){this -= 1; return *this;}
+		int_intern<Ti, To> operator++(int){int_intern<Ti, To> temp = *this; *this += 1; return temp;}
+		int_intern<Ti, To> operator--(int){int_intern<Ti, To> temp = *this; *this -= 1; return temp;}
+		int_intern<Ti, To> operator~(){return ~__getinterno();}
+		int_intern<Ti, To> operator!(){return !__getinterno();}
+#define K9_INTERNAL_INT_CONV(ti, to)\
+		operator int_intern<ti, to>(){\
+			return int_intern<ti, to>(__getintern());\
+		}
+#define K9_INTERNAL_LITERAL_INT_CONV(ti)\
+		operator ti(){\
+			return (ti)(__getintern());\
+		}
+
+K9_INTERNAL_INT_CONSTRUCTOR(uint8_t)
+K9_INTERNAL_INT_CONSTRUCTOR(uint16_t)
+K9_INTERNAL_INT_CONSTRUCTOR(uint32_t)
+K9_INTERNAL_INT_CONSTRUCTOR(int8_t)
+K9_INTERNAL_INT_CONSTRUCTOR(int16_t)
+K9_INTERNAL_INT_CONSTRUCTOR(int32_t)
+#if K9_IMPLEMENTATION_BITS >= 64
+		K9_INTERNAL_INT_CONSTRUCTOR(size_t);
+		K9_INTERNAL_INT_CONSTRUCTOR(ssize_t);
+#endif
+#if K9_IMPLEMENTATION_BITS > 64
+		K9_INTERNAL_INT_CONSTRUCTOR(uint64_t)
+		K9_INTERNAL_INT_CONSTRUCTOR(int64_t)
+#endif
 };
 
-*/
+
+typedef int_intern<uint32_t, int32_t> i32;
+typedef int_intern<uint16_t, int16_t> i16;
+typedef int_intern<uint8_t, int8_t> i8;
+typedef int_intern<size_t, ssize_t> imax;
+typedef int_intern<uint32_t, uint32_t> u32;
+typedef int_intern<uint16_t, uint16_t> u16;
+typedef int_intern<uint8_t, uint8_t>   u8;
+typedef int_intern<size_t, size_t> umax;
+static_assert(sizeof(umax) == sizeof(size_t), "Bad integer types");
+static_assert(sizeof(int8_t) == sizeof(i8), "Bad integer types");
+static_assert(sizeof(uint8_t) == sizeof(u8), "Bad integer types");
+
+#if K9_IMPLEMENTATION_BITS >= 64
+typedef int_intern<uint64_t, int64_t> i64;
+typedef int_intern<uint64_t, uint64_t> u64;
+#endif
 
 template <typename T>
 struct float_intern{
@@ -39,7 +140,7 @@ struct float_intern{
 		 static inline bool isnormal(T a){return std::isnormal(a);}
 	public:
 		float_intern() = default;
-		float_intern(const T other){f = other;}
+		float_intern(const T other) {f = other;}
 		void operator=(const float_intern<T> other) {f = other.__getintern();}
 		void operator=(const T other) {f = other;}
 		T __getintern() const {return f;}
@@ -59,7 +160,7 @@ struct float_intern{
 		K9_INTERINAL_FLOAT_SIMP_OP(-)
 		K9_INTERINAL_FLOAT_SIMP_OP(*)
 		K9_INTERINAL_FLOAT_DIV_OP(/)
-		 float_intern<T> operator%(const float_intern<T> c){
+		float_intern<T> operator%(const float_intern<T> c){
 			if((sizeof(T) == 4))
 				return (fmodf(validate(*this).__getintern() , nonzero(c).__getintern()));
 			if((sizeof(T) == 8))
@@ -87,84 +188,13 @@ struct float_intern{
 		}
 };
 
-//Signed integer implementation.
-template <typename Ti, typename To>
-struct int_intern{
-	static_assert(sizeof(Ti) == sizeof(To));
-	private:
-		Ti f;
-		static inline int_intern<Ti, To> nonzero(int_intern<Ti, To> a){
-			if(a.__getintern() == 0)
-				a.__setintern(0);
-			return a;
-		}
-	public:	
-		int_intern<Ti, To>() = default;
-		int_intern<Ti, To>(To in){*this = in;}
-		int_intern<Ti, To>(Ti in){*this = in;}
-		void operator=(const int_intern<Ti, To> other) 		{f = other.__getintern();}
-		void operator=(const To other) 						{f = (Ti)(other);}
-		Ti __getintern() const 								{return 	f;}
-		To __getinterno() const 							{return (To)(f);}
-		void __setintern(Ti a) 								{f = a;}
-		void __setinterno(To a)								{f = (Ti)(a);}
-	//Actual operator implementations.
-#define K9_INTERNAL_INT_SIMP_OP_NOEQ(x) \
-		int_intern<Ti, To> operator x(const int_intern<Ti, To> c){\
-			return __getintern() x c.__getintern();\
-		}
-#define K9_INTERNAL_INT_SIMP_OP(x) \
-		K9_INTERNAL_INT_SIMP_OP_NOEQ(x)\
-		int_intern<Ti, To> operator x##=(const int_intern<Ti, To> c){*this = *this x c; return *this;}
-#define K9_INTERNAL_INT_CMP_OP_NOEQ(x) \
-		bool operator x(const int_intern<Ti, To> c){\
-			return __getinterno() x (c).__getinterno();\
-		}
-#define K9_INTERNAL_INT_CMP_OP(x) \
-		K9_INTERNAL_INT_CMP_OP_NOEQ(x)\
-		bool operator x##=(const int_intern<Ti, To> c){bool result = *this x c; return result;}
-#define K9_INTERNAL_INT_DIV_OP(x) \
-		int_intern<Ti, To> operator x(const int_intern<Ti, To> c){\
-			return __getinterno() x nonzero(c).__getinterno();\
-		}\
-		int_intern<Ti, To> operator x##=(const int_intern<Ti, To> c){*this = *this x c; return *this;}
-		
-		K9_INTERNAL_INT_SIMP_OP(+)
-		K9_INTERNAL_INT_SIMP_OP(-)
-		K9_INTERNAL_INT_SIMP_OP(*)
-		K9_INTERNAL_INT_SIMP_OP(&)
-		K9_INTERNAL_INT_SIMP_OP(|)
-		K9_INTERNAL_INT_SIMP_OP(^)
-		K9_INTERNAL_INT_SIMP_OP(<<)
-		K9_INTERNAL_INT_SIMP_OP(>>)
-		K9_INTERNAL_INT_SIMP_OP_NOEQ(||)
-		K9_INTERNAL_INT_SIMP_OP_NOEQ(&&)
-		K9_INTERNAL_INT_CMP_OP_NOEQ(==)
-		K9_INTERNAL_INT_CMP_OP_NOEQ(!=)
-		K9_INTERNAL_INT_CMP_OP(>)
-		K9_INTERNAL_INT_CMP_OP(<)
-		K9_INTERNAL_INT_DIV_OP(/)
-		K9_INTERNAL_INT_DIV_OP(%)
-		//int_intern<Ti, To> operator==(int_intern<Ti, To> a){return a.__getinterno() == __getinterno();}
-		int_intern<Ti, To> operator++(){this += 1; return *this;}
-		int_intern<Ti, To> operator--(){this -= 1; return *this;}
-		int_intern<Ti, To> operator++(int){auto temp = *this; *this += 1; return temp;}
-		int_intern<Ti, To> operator--(int){auto temp = *this; *this -= 1; return temp;}
-		int_intern<Ti, To> operator~(){return ~__getinterno();}
-		int_intern<Ti, To> operator!(){return !__getinterno();}
-		//operator int_intern<Ti, Ti>(){int_intern<Ti, Ti> a; a.__setintern(__getintern()); return a;}
-};
-
 
 
 typedef float_intern<float> f32;
-typedef int_intern<uint32_t, int32_t> i32;
-typedef int_intern<uint16_t, int16_t> i16;
-typedef int_intern<uint8_t, int8_t> i8;
 
-typedef int_intern<uint32_t, uint32_t> u32;
-typedef int_intern<uint16_t, uint16_t> u16;
-typedef int_intern<uint8_t, uint8_t>   u8;
+
+
+typedef u8 BYTE;
 static_assert(sizeof(float) == 4);
 static_assert(sizeof(f32) == 4);
 
@@ -180,41 +210,97 @@ static_assert(sizeof(long double) == 16);
 static_assert(sizeof(f128) == 16);
 #endif
 
-#if K9_IMPLEMENTATION_BITS >= 64
 
-#endif
 
-template <size_t size> class state_interface {
+template <size_t lev, size_t size>
+static inline umax fix_index(umax index){
+	static const umax mask = (((umax)1<<(size-1))/((umax)1<<(umax)(lev-1)))-1;
+	index &= mask;
+	return index;
+}
+
+//Bus!
+template <size_t size> 
+class k9_bus{
+	protected:
 	public:
-		template<size_t lev> state_interface<lev>& at(size_t index);
-		virtual BYTE read(size_t where);
-		virtual void write(uint8_t byte, size_t where);
-		void copy(state_interface<size>& other){
-			for(ssize_t i = 0; i < ((size_t)1<<(size-1)); i++) 
+		k9_bus() = default;
+		void fill(BYTE val){
+			for(umax i = 0; i < (umax{1} << umax(size - 1)); i++) {write(val,i);}
+		}
+		virtual BYTE read(umax where);
+		virtual void write(BYTE byte, umax where);
+		void copy(k9_bus<size>& other){
+			for(umax i = 0; i < (umax{1} << umax(size - 1)); i++)
 				{write(other.read(i),i);}
 		}
-		template <size_t lvl> state_interface<lvl> get();
-		state_interface<size-1> high();
-		state_interface<size-1> low();
-		void operator=(state_interface<size>& other){ copy(other); }
+		template<size_t lev> 
+		k9_bus<lev> at(umax index);
+		k9_bus<size-1> high() 	{return at<size-1>(0);}
+		k9_bus<size-1> low() 	{return at<size-1>(1);}
+		void operator=(k9_bus<size>& other){ copy(other); }
 		template <typename T>
 		T to_(){
 			BYTE data[sizeof(T)];
-			for(size_t i = 0; i < sizeof(T); i++){
+			for(umax i = 0; i < umax{sizeof(T)}; i++){
 				data[i] = read(i);
 			}
 			T var;
 			memcpy(&var, data, sizeof(T));
 			return var;
 		}
-		f32 to_f32(){return to_<f32>();}
-#if K9_USE_DOUBLE
-		f64 to_f64(){return to_<f64>();}
-#endif
-#if K9_USE_LONG_DOUBLE
-		f128 to_f128(){return to_<f128>();}
-#endif
-		virtual ~state_interface();
+		template <typename T>
+		void from_(T a){
+			for(size_t i = 0; i < sizeof(T); i++){
+				write( *((BYTE*)&a), i );
+			}
+		}
+		template <typename T>
+		void fromp_(T* a){
+			for(size_t i = 0; i < sizeof(T); i++){
+				write( *((BYTE*)a), i );
+			}
+		}
+		virtual ~k9_bus(){}
 };
-f32 myfunc(f32 a, f32 b){  a++; a %= b; return a; }
-i32 myfunc2(i32 a, i32 b){i32 q; a++; q = 7; a+=q; return a/b; }
+
+template <size_t size> 
+class k9_ptrmem : public k9_bus<size>{
+	public:
+		k9_ptrmem<size>(BYTE* p) : data(p) {}
+		inline BYTE read(umax where) {		  return  data[where & ((umax)1<<(size-1))];	}
+		inline void write(BYTE byte, umax where) {	data[where & ((umax)1<<(size-1))] = byte;}
+		template<size_t lev> k9_ptrmem<lev> at(umax index){
+			k9_ptrmem<lev> k(data + fix_index<lev, size>(index) );
+			return k;
+		}
+	private:
+		BYTE* data;
+};
+
+//MY WORDS ARE BACKED WITH... real memory...
+template <size_t size> 
+class k9_realmem : public k9_bus<size>{
+	public:
+		inline BYTE read(size_t where) {		return  data[fix_index<1,size>(where)];		}
+		inline void write(BYTE byte, size_t where) {	data[fix_index<1,size>(where)] = byte;	}
+		template<size_t lev> k9_ptrmem<lev> at(umax index){
+			k9_ptrmem<lev> k(data + fix_index<lev, size>(index) );
+			return k;
+		}
+		operator k9_ptrmem<size>() {return k9_ptrmem<size>(data);}
+	private:
+		BYTE data[(size_t)1<<(size-1)];
+};
+f32 myfunc(f32 a, f32 b) {a++; a %= b; return a; }
+u32 myfunc2(u32 a, i32 b){ b = 0; i32 q; a++;    q = 7;     a+=q;     return a%b; }
+static k9_realmem<10> mymem;
+
+void myfunc(){
+	k9_realmem<11> some_memory;
+	some_memory.fill(0);
+	some_memory.at<3>(39).from_<u32>(0x7f3f3f7f);
+	std::cout << "Value at the 39th state3, byte 0 is..." << 
+		(uint8_t)some_memory.at<3>(39).read(0) << 
+	std::endl;
+}
